ef434661401209fec40878a4079990e0aac71082
Alymcc <144562869+Alymcc@users.noreply.github.com>
2025-01-22 19:16:52 -0500

    Prototype 1 for real?

--- Template/script.js
+++ Template/script.js
@@ -10,6 +10,7 @@ const canvas =document.querySelector('.webgl')
 
  //Scene
  const scene = new THREE.Scene()
+ scene.background = new THREE.Color('blue')
 
  //Camera
  const camera = new THREE.PerspectiveCamera(
@@ -19,5 +20,47 @@ const canvas =document.querySelector('.webgl')
     100
  )
  scene.add(camera)
+ cancelAnimationFrame.position.set(0, 0, 5)
 
  //Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: camera
+    antialias: true
+})
+renderer,setSize(window.innerWidth,window.innerHeight)
+
+
+
+/****************
+ *** MESHES   ***
+****************/
+
+// testSphere
+const sphereGeometry = new THREE.SphereGeometry(1)
+const sphereMaterial = new THREE.MeshNormalMaterial(1)
+const testSphere = new THREE(sphereGeometry, sphereMaterial)
+
+scene.add(testSphere)
+
+
+/***********************
+ ***  ANIMATION LOOP ***
+ **********************/
+ const clock = new THREE.clock()
+
+ const animation = () =>
+ {
+    // Return elapsedTime
+    const elapsedTime = clock.getElapsedTime()
+    console.log(elapsedTime)
+
+    // Renderer
+    renderer.render(scene,camera)
+
+    // Request next frame
+
+window.requestAnimationFrame(animation)
+
+ }
+
+ animation()
